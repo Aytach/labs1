84592e953350baff8e70eb0860a8aea45b79fcab
Aytach <aytac1212all@gmail.com>
2019-02-26 20:31:46 +0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a,b;
+
+    cout<<"¬ведите первое число: "<<endl;
+    cin>>a;
+
+    cout<<"¬ведите второе число: "<<endl;
+    cin>>b;
+
+    cout<<"¬ведено: "<<a<<"  "<<b<<endl;
+
+    return 0;
+}
